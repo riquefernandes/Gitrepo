58777abb05af317e4297d1069a22fe871451a821
riquefernandes <henriquefernandesleite@gmail.com>
2024-08-03 12:52:43 -0300

    feat: concat name

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ function soma(n1,n2){
 }
 
 console.log(soma(1,2))
+
+
+function concat(nome1, nome2){
+    return `O meu nome é ${nome1} e gosto de ${nome2}`
+
+}
+
+
+const n1 = "Henrique"
+const n2 = "Gustavo"
+
+console.log(concat(n1,n2))
